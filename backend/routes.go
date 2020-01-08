899e6048f607677cc33f0cf0261c7688e54fd03a
Viktor Franzén <viktor_franzen@hotmail.com>
2020-01-08 22:33:21 +0100

    door open handler start.

--- backend/routes.go
+++ backend/routes.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+	"github.com/pkg/errors"
+	"github.com/vifraa/opencbs/cbs"
 )
 
 func (s *server) routes() {
@@ -17,6 +19,8 @@ func (s *server) routes() {
 
 	s.router.Get("/", s.handleIndex())
 
+	s.router.Post("/door", s.handleDoorOpen())
+
 	//exerciseStore := SqlExerciseStore{
 	//	db: s.db,
 	//}
@@ -28,6 +32,39 @@ func (s *server) handleIndex() http.HandlerFunc {
 	//preparations here, can insert arguments
 	return func(w http.ResponseWriter, r *http.Request) {
 		s.respond(w, r, "Hello World", http.StatusOK)
+	}
+}
+
+func (s *server) handleDoorOpen() http.HandlerFunc {
+	type DoorOpenRequest struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+		DoorID   string `json:"doorId"`
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		doorReq := &DoorOpenRequest{}
+		err := s.decode(w, r, doorReq)
+		if err != nil {
+			s.respond(w, r, errors.Wrap(err, "invalid json"), http.StatusBadRequest)
+		}
+
+		err = cbs.LoginCbs(doorReq.Username, doorReq.Password)
+		if err != nil {
+			s.respond(w, r, err, http.StatusBadRequest)
+		}
+
+		err = cbs.LoginAptusPort()
+		if err != nil {
+			s.respond(w, r, err, http.StatusBadRequest)
+		}
+
+		err = cbs.OpenDoor(doorReq.DoorID)
+		if err != nil {
+			s.respond(w, r, err, http.StatusBadRequest)
+		}
+
+		s.respond(w, r, "Door opened", http.StatusBadRequest)
 	}
 }
 
