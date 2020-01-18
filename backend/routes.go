fa8966b3646d01145de76ecb85e5c490777bb70c
Viktor Franzén <viktor_franzen@hotmail.com>
2020-01-18 17:11:15 +0100

    Login route

--- backend/routes.go
+++ backend/routes.go
@@ -2,11 +2,20 @@ package main
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+	"github.com/gorilla/securecookie"
 	"github.com/vifraa/opencsb/csb"
+	"github.com/vifraa/opencsb/user"
 )
+
+var cookieHandler = securecookie.New(
+	securecookie.GenerateRandomKey(64),
+	securecookie.GenerateRandomKey(32))
+
+const sessionName = "opencsb-session"
 
 func (s *server) routes() {
 	s.router = chi.NewMux()
@@ -18,6 +27,8 @@ func (s *server) routes() {
 
 	s.router.Get("/", s.handleIndex())
 
+	s.router.Post("/login", s.handleLogin())
+
 	// TODO Instead of taking the login info each time in post request, have a login endpoint instead.
 	s.router.Post("/doors/open", s.handleDoorOpen())
 	s.router.Post("/doors", s.handleDoorGetAll())
@@ -27,6 +38,65 @@ func (s *server) handleIndex() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		s.respond(w, r, "Hello World", http.StatusOK)
 	}
+}
+
+func (s *server) handleLogin() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		username := r.FormValue("username")
+		password := r.FormValue("password")
+
+		err := csb.LoginCbs(username, password)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		// TODO encrypt password!
+		u := user.User{
+			Username:  username,
+			Password:  password,
+			LastLogin: time.Now(),
+		}
+		s.userRepository.Update(u)
+
+		setSession(username, w)
+	}
+}
+
+func setSession(username string, w http.ResponseWriter) {
+	v := map[string]string{
+		"username": username,
+	}
+
+	encoded, err := cookieHandler.Encode(sessionName, v)
+	if err != nil {
+		return
+	}
+
+	cookie := &http.Cookie{
+		Name:  sessionName,
+		Value: encoded,
+		Path:  "/",
+	}
+
+	http.SetCookie(w, cookie)
+}
+
+func getUsernameFromCookie(r *http.Request) string {
+	cookie, err := r.Cookie(sessionName)
+	if err != nil {
+		// TODO handle error
+		return ""
+	}
+
+	cv := make(map[string]string)
+	err = cookieHandler.Decode(sessionName, cookie.Value, &cv)
+	if err != nil {
+		// TODO handle error
+		return ""
+	}
+
+	return cv["username"]
 }
 
 func (s *server) handleDoorOpen() http.HandlerFunc {
