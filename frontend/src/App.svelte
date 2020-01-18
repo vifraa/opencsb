42f652fbcd054eb97dc0fcfecd46f3a2bd05b8ad
Viktor Franzén <viktor_franzen@hotmail.com>
2020-01-18 12:08:57 +0100

    Login component started. Css changes.

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,13 +1,16 @@
 <script>
-	export let name;
+    import Login from './Login.svelte'
+    export let name;
+
+    
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+    <Login/>
 </main>
 
 <style>
+    
 	main {
 		text-align: center;
 		padding: 1em;
---
+++ frontend/src/Login.svelte
@@ -0,0 +1,47 @@
+<script>
+
+    async function handleLogin(event) {
+        console.log(event)
+        console.log(event.target.username.value)
+        console.log(event.target.password.value)
+    }
+
+</script>
+
+
+<div class="login-container">
+    <h1>Login</h1>
+    <form on:submit|preventDefault="{handleLogin}"
+        class="d-flex flex-column justify-content-center align-items-center">
+        <input type="text" id="username" required placeholder="Username">
+        <input type="password" id="password" required placeholder="Password">
+        <button type="submit" class="btn btn-primary">Login</button>
+    </form>
+</div>
+
+<style>
+    .login-container {
+        background-color: #ffffff;
+        width: fit-content;
+        padding: 30px;
+        padding: 15 15 30 30;
+        margin: auto;
+
+    }
+
+    h1 {
+        font-size: 26px;
+        text-transform: uppercase;
+    }
+
+    button {
+        width: 100%;
+    }
+
+    .login-container>* , form>*{
+        margin-bottom: 8px;
+    }
+
+</style>
+
+
